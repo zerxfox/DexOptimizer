28acf8e9d294d19a6527f51d1d98db855d10b371
zerxfox <36778239+zerxfox@users.noreply.github.com>
2024-06-30 00:36:52 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,50 +3,9 @@ DEVNAME="Magellan, ZerxFox, Гоша"
 MODREQ="Android 9+ & Magisk 23+"
 DEVLINK="@ZerxFox"
 LINKPUB="t.me/mkamhub"
-
 LANG=$(settings get system system_locales)
-unzip -o "$ZIPFILE" 'addon/*' -d $TMPDIR >&2
-. $TMPDIR/addon/Volume-Key-Selector/install.sh
-if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
-    ui_print " | 1: everything-profile: включает профилирование при компиляции" 
-    ui_print " | всего приложения. Профиль выявляет узкие места в коде," 
-    ui_print " | которые могут быть оптимизированы для улучшения производительности."
-    ui_print " | "
-    ui_print " | 2: speed-profile: сконцентрирован на оптимизации скорости выполнения кода."
-    ui_print " | Может быть полезно, если ваша основная цель - максимально ускорить работу приложений."
-    ui_print " | "
-    ui_print " | Выберите профиль [vol+ изменить | vol- выбрать]"
-else
-    ui_print " | 1: everything-profile: enables profiling when compiling"
-    ui_print " | the entire application. The profile identifies bottlenecks"
-    ui_print " | in the code that can be optimized to improve performance."
-    ui_print " | "
-    ui_print " | 2: speed-profile: focused on optimizing code execution speed."
-    ui_print " | May be useful if your main goal is to speed up your applications as much as possible"
-    ui_print " | "
-    ui_print " | Select profile [vol+ change | vol- select]"
-fi
-
-DVal=1
-while true; do
-	ui_print "  $DVal"
-	"$VKSEL" && DVal="$((DVal + 1))" || break
-	[[ "$DVal" -gt "2" ]] && DVal=1
-done
-
-case $DVal in
-	1 ) echo "1" > "$MODPATH/settings.cfg";;
-	2 ) echo "2" > "$MODPATH/settings.cfg";;
-esac
-
-set_permissions() {
-  set_perm_recursive $MODPATH 0 0 0755 0644
-  set_perm $MODPATH/service.sh 0 0 0777
-}
 
 if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
-    ui_print " |"
-    ui_print " | Выбрано: $DVal"
     ui_print " |"
     ui_print " | Модуль: $MODNAME"
     ui_print " |"
@@ -65,8 +24,6 @@ if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
     ui_print " | TG-канал: $LINKPUB"
 else
     ui_print " |"
-    ui_print " | Selected: $DVal"
-    ui_print " |"
     ui_print " | Module: $MODNAME"
     ui_print " |"
     ui_print " | Information block"
@@ -83,5 +40,90 @@ else
     ui_print " |"
     ui_print " | TG-channel: $LINKPUB"
 fi
-ui_print ""
-ui_print ""
+ui_print " |"
+ui_print " |"
+
+unzip -o "$ZIPFILE" 'addon/*' -d $TMPDIR >&2
+. $TMPDIR/addon/Volume-Key-Selector/install.sh
+
+if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
+    ui_print " | 1: everything-profile:выявляет узкие места в коде," 
+    ui_print " | которые могут быть оптимизированы для улучшения"
+    ui_print " | производительности."
+    ui_print " |"
+    ui_print " | 2: speed-profile: оптимизирует скорость выполнения"
+    ui_print " | работы приложений, улучшает отклик интерфейса."
+    ui_print " |"
+    ui_print " | Выберите профиль [vol+ изменить | vol- выбрать]"
+else
+    ui_print " | 1: everything-profile: identifies bottlenecks in the"
+    ui_print " | code, which can be optimized to improve performance."
+    ui_print " |"
+    ui_print " | 2: speed-profile: optimizes the execution speed of"
+    ui_print " | applications, enhances interface responsiveness."
+    ui_print " |"
+    ui_print " | Select profile [vol+ change | vol- select]"
+fi
+
+DVal=1
+while true; do
+	ui_print "  $DVal"
+	"$VKSEL" && DVal="$((DVal + 1))" || break
+	[[ "$DVal" -gt "2" ]] && DVal=1
+done
+
+case $DVal in
+	1 ) echo "1" > "$MODPATH/settings.cfg";;
+	2 ) echo "2" > "$MODPATH/settings.cfg";;
+esac
+
+ui_print " |"
+if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
+    ui_print " | Выбрано: $DVal"
+else
+    ui_print " | Selected: $DVal"
+fi
+
+ui_print " |"
+if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
+    ui_print " | 1: Пользовательские приложения" 
+    ui_print " |"
+    ui_print " | 2: Системные приложения"
+    ui_print " |"
+    ui_print " | 3: Все приложения"
+    ui_print " |"
+    ui_print " | Выберите приложения [vol+ изменить | vol- выбрать]"
+else
+    ui_print " | 1: User apps"
+    ui_print " |"
+    ui_print " | 2: System apps"
+    ui_print " |"
+    ui_print " | 3: All apps"
+    ui_print " |"
+    ui_print " | Select apps [vol+ change | vol- select]"
+fi
+
+DVal=1
+while true; do
+	ui_print "  $DVal"
+	"$VKSEL" && DVal="$((DVal + 1))" || break
+	[[ "$DVal" -gt "3" ]] && DVal=1
+done
+
+case $DVal in
+	1 ) echo "1" > "$MODPATH/packages.cfg";;
+	2 ) echo "2" > "$MODPATH/packages.cfg";;
+    3 ) echo "3" > "$MODPATH/packages.cfg";;
+esac
+
+ui_print " |"
+if [[ "$LANG" =~ "en-RU" ]] || [[ "$LANG" =~ "ru-" ]]; then
+    ui_print " | Выбрано: $DVal"
+else
+    ui_print " | Selected: $DVal"
+fi
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm $MODPATH/service.sh 0 0 0777
+}
